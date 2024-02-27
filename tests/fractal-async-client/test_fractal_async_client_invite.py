ad60fcdd6bb0d875a680442c46c906c1223a1d32
cadea <cadea@CAgostinelli>
2024-02-27 11:54:51 -0600

    Finished testing utils.py and all of its functions. Added another test case that was missing in async client invite function. With those changes, async client and utils are fully tested

--- tests/fractal-async-client/test_fractal_async_client_invite.py
+++ tests/fractal-async-client/test_fractal_async_client_invite.py
@@ -57,6 +57,16 @@ async def test_invite_send_invite():
         )
 
 
+async def test_invite_roominviteerror():
+    sample_user_id = "@sample_user:sample_domain"
+    sample_room_id = "sample_id"
+    client = FractalAsyncClient()
+    client.room_invite = AsyncMock(return_value=RoomInviteError("Room Invite Failed"))
+    with pytest.raises(Exception) as e:
+        await client.invite(user_id=sample_user_id, room_id=sample_room_id, admin=True)
+    assert "Room Invite Failed" in str(e.value)
+
+
 async def test_invite_raise_exception_for_userID():
     sample_user_id = "sample_user:sample_domain"
     sample_room_id = "sample_id"
@@ -100,7 +110,6 @@ async def test_invite_room_get_state_event_error_when_has_message():
     assert "Error message" in str(e.value)
 
 
-# @pytest.mark.skip("Having trouble reaching the else condition and testing the exception")
 async def test_invite_room_get_state_event_error_when_no_message():
     sample_user_id = "@sample_user:sample_domain"
     sample_room_id = "sample_id"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,68 @@
-from getpass import getpass
-from io import StringIO
-from unittest.mock import patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 from fractal.matrix import utils
 from fractal.matrix.exceptions import InvalidMatrixIdException
 
 
-@pytest.mark.skip()
-async def test_prompt_matrix_password():
+async def test_prompt_matrix_password_if_homeserver_url(capsys):
     matrix_id = "test_matrix_id"
-    expected_password = "test_password"
-    utils.prompt_matrix_password = AsyncMock(return_value=expected_password)
+    homeserver_url = "homeserver_url"
+    sample_password = "sample_password"
+    with patch("fractal.matrix.utils.getpass", new=MagicMock(return_value=sample_password)):
+        utils.prompt_matrix_password(matrix_id=matrix_id, homeserver_url=homeserver_url)
+        text = capsys.readouterr()
+    assert (
+        f"Login with Matrix ID ({matrix_id}) to sign in to {homeserver_url}" == text.out.strip()
+    )
+
+
+async def test_prompt_matrix_password_if_no_homeserver_url(capsys):
+    matrix_id = "test_matrix_id"
+    sample_password = "sample_password"
+    with patch("fractal.matrix.utils.getpass", new=MagicMock(return_value=sample_password)):
+        utils.prompt_matrix_password(matrix_id=matrix_id)
+        text = capsys.readouterr()
+    assert f"Login with Matrix ID ({matrix_id}) to continue" == text.out.strip()
+
+
+async def test_prompt_matrix_password_no_interrupt():
+    matrix_id = "test_matrix_id"
+    sample_password = "sample_password"
+    with patch("fractal.matrix.utils.getpass", new=MagicMock(return_value=sample_password)):
+        password = utils.prompt_matrix_password(matrix_id)
+    assert password == sample_password
 
 
 async def test_prompt_matrix_password_keyboard_interrupt():
     matrix_id = "test_matrix_id"
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        with patch("builtins.getpass", side_effect=KeyboardInterrupt):
+    with pytest.raises(SystemExit) as e:
+        with patch("fractal.matrix.utils.getpass", new=MagicMock(side_effect=KeyboardInterrupt)):
             utils.prompt_matrix_password(matrix_id)
-    assert pytest_wrapped_e.type == SystemExit
+    assert e.type == SystemExit
+
+
+async def test_parse_matrix_id_group_returns():
+    matrix_id = "test_matrix_id"
+    sample_localpart = "test_localpart"
+    sample_homeserver = "test_homeserver"
+    compiled_pattern_mock = MagicMock()
+    match_mock = MagicMock()
+    compiled_pattern_mock.match.return_value = match_mock
+    with patch("re.compile", return_value=compiled_pattern_mock):
+        with patch.object(
+            match_mock, "group", return_value=(sample_localpart, sample_homeserver)
+        ):
+            localpart, homeserver = utils.parse_matrix_id(matrix_id)
+    assert localpart[0] == sample_localpart
+    assert homeserver[1] == sample_homeserver
+
+
+async def test_parse_matrix_id_invalidmatrixidexception():
+    matrix_id = "test_matrix_id"
+    compiled_pattern_mock = MagicMock()
+    compiled_pattern_mock.match.return_value = None
+    with patch("re.compile", return_value=compiled_pattern_mock):
+        with pytest.raises(InvalidMatrixIdException) as e:
+            utils.parse_matrix_id(matrix_id)
+        assert f"{matrix_id} is not a valid Matrix ID." in str(e.value)
